e933906e24229921a4debad1266ab978249d4476
Emil <emil.schmidt03@gmail.com>
2025-09-25 11:38:33 +0200

    Added functionality to create and draw bounding boxes for the estimated position of axles above the clustered points

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pypcd4 import PointCloud
 from sklearn.cluster import DBSCAN
+import matplotlib.patches as mpatches
 
 POINTCLOUD_PATH = "/Users/emil/Documents/HSE_VSV/Project_Axle-Detection/Pointclouds/lidar_point_cloud_3/transit_20250922-100144_c500_id10561.pcd"
-MIN_CLEARENCE = 0.06 # 11.5 cm by "law" but mudflaps exist :(
-DBSCAN_EPSILON = 0.15 # scanlines are about 12 cm apart
-DBSCAN_MIN_SAMPLES = 2 # needs two points for a bounding box
+MIN_CLEARENCE = 0.06  # 11.5 cm by "law" but mudflaps exist :(
+DBSCAN_EPSILON = 0.15  # scanlines are about 12 cm apart
+DBSCAN_MIN_SAMPLES = 2  # needs two points for a bounding box
 
 
 def read_pointcloud(path=POINTCLOUD_PATH):
@@ -36,17 +37,37 @@ def get_lower_wheel_points(pointcloud_array):
     lower_wheel_points = lower_bounds[wheel_mask]
     return lower_wheel_points
 
+
 def get_point_cluster(lower_wheel_points):
     dbscan = DBSCAN(eps=DBSCAN_EPSILON, min_samples=DBSCAN_MIN_SAMPLES)
-    clusters = dbscan.fit_predict(lower_wheel_points)
-    return clusters
+    labels = dbscan.fit_predict(lower_wheel_points)
+    return labels
+
+
+def get_bounding_boxes(points, labels):
+    unique_labels = np.unique(labels)
+    clusters = [points[labels == l] for l in unique_labels]
+
+    boxes = {}
+    for label, cluster in zip(unique_labels, clusters):
+        max_y = cluster[:, 1].max()
+        min_x = cluster[:, 0].min()
+        max_x = cluster[:, 0].max()
+
+        anchor = (min_x, max_y)
+        edge_length = max_x - min_x
+        boxes[label] = (anchor, edge_length)
+
+    return boxes
 
 
 if __name__ == "__main__":
     pointcloud_array = read_pointcloud()
     lower_bounds = get_lower_bounds(pointcloud_array)
     lower_wheel_points = get_lower_wheel_points(pointcloud_array)
-    clusters = get_point_cluster(lower_wheel_points)
+    labels = get_point_cluster(lower_wheel_points)
+    boxes = get_bounding_boxes(lower_wheel_points, labels)
+    print(boxes)
 
     plt.style.use("dark_background")
     fig, ax = plt.subplots()
@@ -73,11 +94,20 @@ if __name__ == "__main__":
         lower_wheel_points[:, 0],
         lower_wheel_points[:, 1],
         marker="X",
-        c=clusters,
+        c=labels,
         cmap="autumn",
         label="Wheel points",
         zorder=10,
-    )  # plot for the detected wheel points
+    )  # plot for the detected wheel points (with cluster colouring)
+    for lbl, ((anchor_x, anchor_y), edge_length) in boxes.items():
+        rect = mpatches.Rectangle(
+            (anchor_x, anchor_y),
+            width=edge_length,
+            height=edge_length,
+            fill=False,
+            zorder=15,
+        )
+        ax.add_patch(rect)
 
     plt.xlabel("y")
     plt.ylabel("z")
